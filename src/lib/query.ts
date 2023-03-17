bc667b7e0ca4fb7ed2904107a77e2cdb4d0d26c6
tglide <26071571+TGlide@users.noreply.github.com>
2023-03-17 21:13:04 +0000

    :sparkles: advanced options POC

--- src/lib/query.ts
+++ src/lib/query.ts
@@ -1,7 +1,55 @@
 import type { IconName } from '$UI/icon.svelte';
 import type { Lang } from 'shiki';
 
-type QueryFn = (input: string) => string;
+type QueryFn = (input: string, params?: Record<string, string | boolean>) => string;
+
+function createQueryFn(baseInstructions: string): QueryFn {
+	return (input, params) => {
+		if (!params) return baseInstructions + `\n\nHere's the code:\n${input}`;
+		const stringifiedParams = Object.entries(params).map(([key, value]) => `${key}: ${value}`);
+		return (
+			baseInstructions +
+			`\n\nUse the following parameters:\n` +
+			stringifiedParams.join('\n') +
+			`\n\nHere's the code:\n${input}`
+		);
+	};
+}
+
+export function getParamsFromForm(
+	form: HTMLFormElement,
+	params: Record<string, Parameter>
+): Record<string, string | boolean> | undefined {
+	const result: Record<string, string | boolean> = {};
+	const formData = new FormData(form);
+
+	for (const [key, value] of formData) {
+		console.log(key, value);
+		if (key in params === false) continue;
+		const param = params[key];
+		if (param.type === 'boolean') {
+			result[key] = value === 'on';
+		} else {
+			result[key] = value as string;
+		}
+	}
+
+	if (Object.keys(result).length === 0) return undefined;
+	return result;
+}
+
+type BooleanParameter = {
+	type: 'boolean';
+};
+
+type StringParameter = {
+	type: 'string';
+	values: string[];
+};
+
+type Parameter = (BooleanParameter | StringParameter) & {
+	label: string;
+};
 
 type Options = {
 	[value: string]: {
@@ -9,6 +57,7 @@ type Options = {
 		icon: IconName;
 		lang: Lang;
 		query: QueryFn;
+		params?: Record<string, Parameter>;
 	};
 };
 
@@ -21,89 +70,98 @@ export const queryOptions = {
 		label: 'Svelte',
 		icon: 'svelte',
 		lang: 'svelte',
-		query: (input) => `Convert the following component to a Svelte component.
-			Don't import useState, as Svelte does not have it. Use Typescript.
-			If onMount is present, do not use onDestroy. Instead, return a cleanup function from onMount.
-			Do not use createRef. Here's the component:\n${input}`
+		query: createQueryFn(`Convert the following component to a Svelte component.
+      Don't import useState, as Svelte does not have it.
+      If onMount is present, do not use onDestroy. Instead, return a cleanup function from onMount.
+      Do not use createRef.`),
+		params: {
+			useSvelteKit: {
+				type: 'boolean',
+				label: 'Use SvelteKit'
+			},
+			useTypeScript: {
+				type: 'boolean',
+				label: 'Use TypeScript'
+			},
+			stylingSolution: {
+				type: 'string',
+				label: 'Styling solution',
+				values: ['CSS', 'SCSS', 'PostCSS', 'Tailwind CSS']
+			}
+		}
 	},
 	react: {
 		label: 'React',
 		icon: 'react',
 		lang: 'tsx',
-		query: (input) =>
-			`Convert the following component to a React component
-				Show the component directly, without wrapping it in 'jsx' quotes:\n${input}`
+		query: createQueryFn(`Convert the following component to a React component.`)
 	},
 	vue2: {
 		label: 'Vue 2',
 		icon: 'vue',
 		lang: 'vue',
-		query: (input) => `Convert the following component to a Vue 2 component:\n${input}`
+		query: createQueryFn('Convert the following component to a Vue 2 component:')
 	},
 	vue3: {
 		label: 'Vue 3',
 		icon: 'vue',
 		lang: 'vue',
-		query: (input) =>
-			`Convert the following component to a Vue 3 component, using SFCs, template tags, and <script setup>.
-			Use refs for state, and defineProps for props (if needed). Do not use useState.
-			Here's the component:\n${input}`
+		query:
+			createQueryFn(`Convert the following component to a Vue 3 component, using SFCs, template tags, and <script setup>.
+      Use refs for state, and defineProps for props (if needed). Do not use useState.`)
 	},
 	angular: {
 		label: 'Angular',
 		icon: 'angular',
 		lang: 'ts',
-		query: (input) => `Convert the following component to an Angular component:\n${input}`
+		query: createQueryFn(`Convert the following component to an Angular component.`)
 	},
 	css: {
 		label: 'CSS',
 		icon: 'css',
 		lang: 'css',
-		query: (input) => `Convert the following tailwind-css code to normal CSS.
-	Use the theme function when possible, e.g. '@apply text-white' should become 'color: theme("colors.white")''. 
-	Also, be smart with opacity colors. e.g. '@apply bg-black-50' should become 'background-color: theme("colors.black/0.5")'.
-	Do not use CSS vars. Do not use hex color values.
-	For hover states, use SCSS-like nesting. e.g. '@apply hover:bg-black-50' should become '&:hover { background-color: theme("colors.black/0.5") }'.
-	This is the code:\n${input}`
+		query: createQueryFn(`Convert the code to normal CSS.
+			If the code is using tailwind CSS then follow these rules:
+			- START TAILWIND RULES- 
+      Use the theme function when possible, e.g. '@apply text-white' should become 'color: theme("colors.white")''. 
+      Also, be smart with opacity colors. e.g. '@apply bg-black-50' should become 'background-color: theme("colors.black/0.5")'.
+      Do not use CSS vars. Do not use hex color values.
+      For hover states, use SCSS-like nesting. e.g. '@apply hover:bg-black-50' should become '&:hover { background-color: theme("colors.black/0.5") }'.
+			- END TAILWIND RULES -`)
 	},
 	tailwind: {
 		label: 'Tailwind',
 		icon: 'tailwind',
 		lang: 'html',
-		query: (
-			input
-		) => `Convert the following CSS code to Tailwind CSS code. Use the @apply directive.
-	e.g. .input { border-radius: theme('borderRadius.md'); background-color: theme('colors.black/0.5');} should become
-	 <input class="rounded-md bg-black/50" />":\n${input}`
+		query:
+			createQueryFn(`Convert the following CSS code to Tailwind CSS code. Use the @apply directive.
+			e.g. .input { border-radius: theme('borderRadius.md'); background-color: theme('colors.black/0.5');} should become
+			 <input class="rounded-md bg-black/50" />"`)
 	},
 	python: {
 		label: 'Python',
 		icon: 'python',
 		lang: 'python',
-		query: (input) => `Convert the following code to Python 3:\n${input}`
+		query: createQueryFn(`Convert the following code to Python 3.`)
 	},
 	javascript: {
 		label: 'JavaScript',
 		icon: 'javascript',
 		lang: 'js',
-		query: (input) => `Convert the following code to JavaScript:\n${input}`
+		query: createQueryFn(`Convert the following code to JavaScript.`)
 	},
 	typescript: {
 		label: 'TypeScript',
 		icon: 'typescript',
 		lang: 'ts',
-		query: (input) => `Convert the following code to TypeScript:\n${input}`
+		query: createQueryFn(`Convert the following code to TypeScript.`)
 	},
 	rust: {
 		label: 'Rust',
 		icon: 'rust',
 		lang: 'rust',
-		query: (input) => `Convert the following code to Rust:\n${input}`
+		query: createQueryFn(`Convert the following code to Rust.`)
 	}
 } satisfies Options;
 
 export const languages = Object.values(queryOptions).map((o) => o.lang);
-
-export function generateQuery(input: string, type: keyof typeof queryOptions) {
-	return queryOptions[type].query(input);
-}
