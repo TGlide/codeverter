38ac561da7dcc28d64acc2a8e49257985daf264e
tglide <26071571+TGlide@users.noreply.github.com>
2023-03-21 11:14:43 +0000

    :recycle: change from plausible to vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,11 @@
 	import '@fontsource/inter/500.css';
 	import '@fontsource/inter/600.css';
 	import '@fontsource/inter/800.css';
+
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <slot />
