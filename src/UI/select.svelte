248a2783a04d3b7e3fd3e5bfc63391b6d2993937
tglide <26071571+TGlide@users.noreply.github.com>
2023-03-21 22:13:49 +0000

    :recycle: clean select styles

--- src/UI/select.svelte
+++ src/UI/select.svelte
@@ -20,14 +20,12 @@
 
 <style lang="postcss">
 	select {
-		@apply focus:border-orange-300 focus:outline-none focus:ring focus:ring-orange-300;
 		position: relative;
 		border: 1px solid theme('colors.gray.500');
 		border-radius: theme('borderRadius.md');
 		background-color: theme('colors.black');
 
 		padding: 0.5rem;
-
 		outline: none;
 
 		&:focus {
